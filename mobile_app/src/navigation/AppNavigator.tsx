d348c0dd56ce9c1e3ab47012c2533dcad6d943fc
Mfth Farid <mfthfarid1826@gmail.com>
2026-03-27 23:40:35 +0700

    fix: navigator

--- mobile_app/src/navigation/AppNavigator.tsx
+++ mobile_app/src/navigation/AppNavigator.tsx
@@ -2,18 +2,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Dashboard from '../screens/Dashboard'; // Layar utama dengan MainLayout
+import Dashboard from '../screens/Dashboard';
 import Notifikasi from '../screens/Notifikasi';
 import { RootStackParamList } from '../types/types';
-// import DetailProduk from '../screens/DetailProduk';
-// import TambahProduk from '../screens/TambahProduk';
-
-// export type RootStackParamList = {
-//   Dashboard: undefined;
-//   DetailProduk: { id: number };
-//   TambahProduk: undefined;
-//   Notifikasi: undefined;
-// };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -21,16 +12,11 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Dashboard">
-        {/* Layar utama dengan MainLayout */}
         <Stack.Screen
           name="Dashboard"
           component={Dashboard}
-          options={{ headerShown: false }} // Sembunyikan header default karena sudah ada di MainLayout
+          options={{ headerShown: false }}
         />
-
-        {/* Layar-layar tambahan dengan header bawaan Stack */}
-        {/* <Stack.Screen name="DetailProduk" component={DetailProduk} options={{ title: 'Detail Produk' }} />
-        <Stack.Screen name="TambahProduk" component={TambahProduk} options={{ title: 'Tambah Produk Baru' }} /> */}
         <Stack.Screen
           name="Notifikasi"
           component={Notifikasi}
