45ae9a702132016c42d339c2720f1cc2ebc9d997
Mfth Farid <mfthfarid1826@gmail.com>
2026-07-13 20:37:10 +0700

    feat: custom konversi tanggal

--- backend_api/internal/features/product/product_dto.go
+++ backend_api/internal/features/product/product_dto.go
@@ -1,21 +1,21 @@
 package product
 
-import "time"
+import "github.com/mfthfarid/TokoPrediksi/backend_api/internal/shared/customtype"
 
 type CreateProductInput struct {
-	Kode               string     `json:"kode" binding:"required"`
-	Name               string     `json:"name" binding:"required,min=3,max=100"`
-	Price              uint       `json:"price" binding:"required,gt=0"`
-	Stock              uint       `json:"stock" binding:"gte=0"`
-	IDKategori         *uint      `json:"id_kategori" binding:"omitempty"`
-	TanggalKadaluwarsa *time.Time `json:"tanggal_kadaluwarsa"`
+	Kode               string           `json:"kode" binding:"required"`
+	Name               string           `json:"name" binding:"required,min=3,max=100"`
+	Price              uint             `json:"price" binding:"required,gt=0"`
+	Stock              uint             `json:"stock" binding:"gte=0"`
+	IDKategori         *uint            `json:"id_kategori" binding:"omitempty"`
+	TanggalKadaluwarsa *customtype.Date `json:"tanggal_kadaluwarsa"`
 }
 
 type UpdateProductInput struct {
-	Kode               *string    `json:"kode" binding:"omitempty"`
-	Name               *string    `json:"name" binding:"omitempty,min=3,max=100"`
-	Price              *uint      `json:"price" binding:"omitempty,gt=0"`
-	Stock              *uint      `json:"stock" binding:"omitempty,gte=0"`
-	IDKategori         *uint      `json:"id_kategori" binding:"omitempty"`
-	TanggalKadaluwarsa *time.Time `json:"tanggal_kadaluwarsa"`
+	Kode               *string          `json:"kode" binding:"omitempty"`
+	Name               *string          `json:"name" binding:"omitempty,min=3,max=100"`
+	Price              *uint            `json:"price" binding:"omitempty,gt=0"`
+	Stock              *uint            `json:"stock" binding:"omitempty,gte=0"`
+	IDKategori         *uint            `json:"id_kategori" binding:"omitempty"`
+	TanggalKadaluwarsa *customtype.Date `json:"tanggal_kadaluwarsa"`
 }
